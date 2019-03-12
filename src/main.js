82c2e0a29413e061ba16ab5108b7c413fa834cbf
h1-the-swan <jason.portenoy@gmail.com>
2019-03-12 11:00:33 -0700

    live data update in vis is finally working

--- src/main.js
+++ src/main.js
@@ -20,14 +20,22 @@ var fuseOptions = {
 	]
 };
 
-d3.json("data/coauthorship_nas2_doilinked_largest_cc.json").then(function(graph) {
-	var nodelinkvis = NodeLinkCoauthorshipVis().width(960).data(graph);
+var nodelinkvis;
+
+d3.json("data/test_coauthorship_graph.json").then(function(graph) {
+	nodelinkvis = NodeLinkCoauthorshipVis().width(960).data(graph);
 	d3.select("#chartDiv").call(nodelinkvis);
 	// nodelinkvis.data(graph);
 
 	activateTooltips();
 
 });
+
+setTimeout(function() {
+	d3.json("data/test_coauthorship_graph_misinfo.json").then(function(graph) {
+		nodelinkvis.data(graph);
+	});
+}, 3000);
 
 function applyRadioSelection() {
 	var val = $( 'input[type=radio][name=nodelab-radio]:checked' ).val();
--- src/vis.js
+++ src/vis.js
@@ -24,6 +24,37 @@ function NodeLinkCoauthorshipVis() {
 	var manyBody = d3.forceManyBody()
 						.strength(-2);
 
+	function getLinkId(link, directed = false) {
+		// link ID will be the concatenation of the source ID and target ID, separated by "-&-"
+		// If the graph is undirected, the IDs will be sorted ALPHABETICALLY using the string values of the ID
+		var source = link.source.toString();
+		var target = link.target.toString();
+		var items = [source, target];
+		var sep = "-&-";
+		if (directed === true) {
+			items.sort();
+		}
+		return items[0] + sep + items[1];
+	}
+
+	function dragstarted(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+	  d.fx = d.x;
+	  d.fy = d.y;
+	}
+
+	function dragged(d) {
+	  d.fx = d3.event.x;
+	  d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0);
+	  d.fx = null;
+	  d.fy = null;
+	}
+
+
 	function chart(selection) {
 		selection.each(function() {
 			var selItem = this;
@@ -31,37 +62,11 @@ function NodeLinkCoauthorshipVis() {
 			var height = .625 * width;
 			var svg = d3.select(selItem).append("svg").attr("width", width).attr("height", height);
 
-			var simulation = d3.forceSimulation()
-				// .force("link", d3.forceLink().id(function(d) { return d.id; }))
-				.force("link", d3.forceLink())
-				.force("charge", manyBody)
-				.force("center", d3.forceCenter(width / 2, height / 2));
-
-			var sizeScale = d3.scaleLinear()
-				.range([4, 12]);
-
-			var graph = data;
-			console.log(graph);
-
-			graph.nodes.forEach(function(d) {
-				d.id = d.id.toString();
-			});
-
-			sizeScale.domain(d3.extent(graph.nodes, function(d) { return d.flow; }));
-
-
-		  var link = svg.append("g")
-			  .attr("class", "links")
-			.selectAll("line")
-			.data(graph.links)
-			.enter().append("line")
-			  .attr("stroke-width", function(d) { return Math.sqrt(d.weight); });
-
-		  var node = svg.append("g")
-			  .attr("class", "nodes")
-			.selectAll(".node")
-			.data(graph.nodes)
-			.enter().append("g")
+			function enterNodes(node) {
+				// pass in the selection for entering nodes
+				
+				
+				var nodeEnter = node.enter().append("g")
 			  // .attr("r", 5)
 				.attr("class", "node")
 			  .call(d3.drag()
@@ -69,34 +74,97 @@ function NodeLinkCoauthorshipVis() {
 				  .on("drag", dragged)
 				  .on("end", dragended));
 
-			var nodeCircle = node.append("circle")
-			  .attr("r", function(d) { return d.radius = sizeScale(d.flow); })
-			  // .attr("fill", function(d) { return d.color_orig = color(d.cl_top); });
-			  .attr("fill", function(d) {
-				  if (d.hasOwnProperty("cl_top")) {
-					  d.color_orig = color(d.cl_top);
-				  } else {
-					  d.color_orig = color(0);
-				  }
-				  return d.color_orig
-			  });
 
-			function dragstarted(d) {
-			  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-			  d.fx = d.x;
-			  d.fy = d.y;
+				node = node.merge(nodeEnter)
+					.each(function(d) {
+						if (d.hasOwnProperty('flow')) {
+						  d.radius = sizeScale(d.flow);
+						} else {
+						  d.radius = 5;
+						}
+					});
+
+			  var nodeCircle = nodeEnter.append("circle")
+				.attr("r", function(d) {
+					return d.radius;
+				})
+				// .attr("fill", function(d) { return d.color_orig = color(d.cl_top); });
+				.attr("fill", function(d) {
+					if (d.hasOwnProperty("cl_top")) {
+						d.color_orig = color(d.cl_top);
+					} else {
+						d.color_orig = color(0);
+					}
+					return d.color_orig
+				});
+				return node;
 			}
 
-			function dragged(d) {
-			  d.fx = d3.event.x;
-			  d.fy = d3.event.y;
+			function enterLinks(link) {
+				// pass in the selection for entering links
+				
+				
+				var linkEnter = link.enter().append("line").attr("class", "link")
+				  .attr("stroke-width", function(d) { return Math.sqrt(d.weight); });
+				link = link.merge(linkEnter);
+				return link;
 			}
 
-			function dragended(d) {
-			  if (!d3.event.active) simulation.alphaTarget(0);
-			  d.fx = null;
-			  d.fy = null;
-			}
+			var graph = data;
+			console.log(graph);
+
+			graph.nodes.forEach(function(d) {
+				d.id = d.id.toString();
+			});
+			graph.links.forEach(function(d) {
+				d.id = getLinkId(d);
+			});
+
+			var simulation = d3.forceSimulation()
+				.force("link", d3.forceLink().id(function(d) { return d.id; }))
+				// .force("link", d3.forceLink())
+				.force("charge", manyBody)
+				.force("center", d3.forceCenter(width / 2, height / 2));
+
+			var sizeScale = d3.scaleLinear()
+				.range([4, 12]);
+
+			sizeScale.domain(d3.extent(graph.nodes, function(d) {
+				if (d.hasOwnProperty('flow')) {
+					return d.flow;
+				} else {
+					return [5,5]
+				}
+			}));
+
+
+		  var link = svg.append("g")
+			  .attr("class", "links")
+			// .selectAll("line")
+			.selectAll(".link")
+			.data(graph.links, function(d) {
+				return d.id;
+			});
+			// .enter();
+			// .append("line").attr("class", "link")
+			//   .attr("stroke-width", function(d) { return Math.sqrt(d.weight); });
+
+		  var node = svg.append("g")
+			  .attr("class", "nodes")
+			.selectAll(".node")
+			.data(graph.nodes, function(d) { return d.id; });
+			// .enter();
+				// .append("g")
+			  // // .attr("r", 5)
+				// .attr("class", "node")
+			  // .call(d3.drag()
+				//   .on("start", dragstarted)
+				//   .on("drag", dragged)
+				//   .on("end", dragended));
+			node = enterNodes(node);
+			link = enterLinks(link);
+
+
 			// node.append("text")
 			// 	.attr("class", "affil_name")
 			// 	.style("display", "none")  // hidden initially
@@ -155,11 +223,11 @@ function NodeLinkCoauthorshipVis() {
 			  .links(graph.links);
 
 		  function ticked() {
-			// node
-			//     .attr("cx", function(d) { return d.x; })
-			//     .attr("cy", function(d) { return d.y; });
+			node
+			    .attr("cx", function(d) { return d.x; })
+			    .attr("cy", function(d) { return d.y; });
 			// add bounding box
-			  // https://bl.ocks.org/mbostock/1129492
+			//   https://bl.ocks.org/mbostock/1129492
 			node
 				.attr("cx", function(d) { 
 					d.x = Math.max(d.radius, Math.min(width - d.radius, d.x));
@@ -186,6 +254,15 @@ function NodeLinkCoauthorshipVis() {
 			}
 			// nodeTooltips();  // not working!!
 			svg.on("click", reset_layout);
+
+			// // TEST
+			// setTimeout(function() {
+			// 	d3.json("data/test_coauthorship_graph_misinfo.json").then(function(graph) {
+			// 		simulation.stop();
+			// 		d3.selectAll('.node').data(graph.nodes, function(d) { return d.id; }).exit().remove();
+			// 		// link.data(graph.links, function(d) { return d.id; }).exit().remove();
+			// 	});
+			// }, 2000);
 
 			// TODO this is broken. fix it.
 			// var fuse = new Fuse(graph.nodes, fuseOptions);
@@ -224,7 +301,59 @@ function NodeLinkCoauthorshipVis() {
 			updateData = function() {
 				// use D3 update pattern with data
 
-				// NOT IMPLEMENTED
+				graph = data;
+				graph.nodes.forEach(function(d) {
+					d.id = d.id.toString();
+				});
+				graph.links.forEach(function(d) {
+					d.id = getLinkId(d);
+				});
+				console.log(graph);
+				// simulation.stop();
+				// var linkExit = link.data(graph.links, function(d) { return d.id; }).exit().remove();
+				// console.log(linkExit);
+				// node = d3.selectAll('.node').data(graph.nodes, function(d) { return d.id; });
+				node = node.data(graph.nodes, function(d) { return d.id; });
+				var nodeExit = node.exit();
+				// nodeExit.selectAll("circle").attr("fill", "red");
+				nodeExit.remove();
+				node = enterNodes(node);
+
+
+				// var orig_ids = [];
+				// link.each(function(d) {
+				// 	orig_ids.push(d.id);
+				// });
+				// link = d3.selectAll('.link')
+				link = link
+					.data(graph.links, function(d) {
+						// d.id = getLinkId(d);
+						return d.id;
+					});
+				var linkExit = link.exit();
+				linkExit.remove();
+				link = enterLinks(link);
+				// console.log(orig_ids);
+				// link.each(function(d) {
+				// 	if (orig_ids.includes(d.id)) {
+				// 		console.log("true");
+				// 	} else {
+				// 		console.log("false");
+				// 	}
+				// });
+				// var linkEnter = link.enter();
+				link.style("stroke", "red");
+				simulation
+					.nodes(graph.nodes)
+					.on("tick", ticked);
+
+				simulation.force("link")
+					.links(graph.links);
+				// for (var i = 0; i < 30; i++) {
+				// 	simulation.tick()
+				// }
+				// console.log("ticked");
+				// simulation.restart();
 			}
 
 
